84c587030df3ee4a241a5efaf4940ec2924e7363
shack1234 <shadrack.anayo@strathmore.edu>
2020-02-22 21:57:55 +0300

    added what we do function ...toggle functions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// 'what we do' functions
+$(document).ready(function () {
+    $(".design_icon").click(function () {
+        $("#design_icon").toggle();
+        $("#design-p").toggle();
+    });
+    $(".dev_icon").click(function () {
+        $("#development").toggle();
+        $("#dev_icon").toggle();
+    });
+    $(".product_icon").click(function () {
+        $("#pmanagent").toggle();
+        $("#product_icon").toggle();
+    });
+});
